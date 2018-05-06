073fb85e9cba66abd4b40559b774c03edcb087e0
DiasFelipe <fdczzz@hotmail.com>
2018-05-06 01:58:14 -0300

    Search for user by id with properly error handler

--- src/main/java/com/workshop/springmongo/services/UserService.java
+++ src/main/java/com/workshop/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.workshop.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.springmongo.domain.User;
 import com.workshop.springmongo.repository.UserRepository;
+import com.workshop.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		if(id.isEmpty())
+			throw new IllegalArgumentException("Id argument connit be empty");
+		Optional<User> user  = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("User not found by id"));
 	}
 }
